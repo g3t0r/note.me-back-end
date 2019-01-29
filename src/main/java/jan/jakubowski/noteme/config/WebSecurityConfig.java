2a3dc0feb611a5a8287e14d0d390a81f3a7b2f90
g3t0r <jan.jakubowski.ti@gmail.com>
2019-01-29 15:41:29 +0100

    Changed registration login in security config

--- src/main/java/jan/jakubowski/noteme/config/WebSecurityConfig.java
+++ src/main/java/jan/jakubowski/noteme/config/WebSecurityConfig.java
@@ -36,7 +36,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/users/register", "/api/login")
+                .antMatchers("/", "/api/users/register", "/api/login")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
